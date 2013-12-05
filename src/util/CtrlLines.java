2945935a466e304f2e70e968c406bc7f35d723fd
hugokallstrom <hugokallstrom@gmail.com>
2013-12-05 13:53:42 +0100

    hk

--- src/util/CtrlLines.java
+++ src/util/CtrlLines.java
@@ -2,4 +2,14 @@ package util;
 
 public class CtrlLines {
 
+	boolean regDist = false;
+	boolean branch = false;
+	boolean memRead = false;
+	boolean memtoReg = false;
+	boolean memWrite = false;
+	boolean regWrite = false;
+	boolean ALUOp = false;
+	boolean ALUSrc = false;
+
+
 }
