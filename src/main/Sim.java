ae5770099386769c513c953a54f56883ffde3fe3
oi11msd <oi11msd@cs.umu.se>
2013-12-06 11:52:57 +0100

    trtrtr

--- src/main/Sim.java
+++ src/main/Sim.java
@@ -1,7 +1,47 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 public class Sim {
 
+	private ArrayList<String> instruction;
+	
+	public Sim(String file) {
+		instruction = new ArrayList<String>();
+		readFile(file);
+		
+	}
+	/**
+	 * readFile - reads the file specified as the input argument "file"
+	 * and puts every line in a list containing strings.
+	 */
+	private void readFile(String file) {
+		String line = null;
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			while((line = br.readLine()) != null) {
+				instruction.add(line);
+			}
+			br.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	private void mnemToConst() {
+		String temp = "";
+		String instructName = "";
+		int i = 0;
+		while((temp = instruction.get(i)) != null) {
+			int index = temp.indexOf(" ");
+			instructName = temp.substring(0, index);
+			
+		}
+	}
+	
+	
 	/**
 	 * @param args
 	 */
@@ -10,4 +50,6 @@ public class Sim {
 
 	}
 
+	
+	
 }
--- src/util/Mappings.java
+++ src/util/Mappings.java
@@ -1,5 +1,15 @@
 package util;
 
+import java.util.HashMap;
+
 public class Mappings {
 
+	public HashMap<String, String> instructionMapping = new HashMap();
+	
+	public Mappings() {
+		instructionMapping.put("add", "R");
+		instructionMapping.put("add", "R");
+		instructionMapping.put("add", "R");
+	}
+	
 }
